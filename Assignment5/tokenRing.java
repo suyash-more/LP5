7f758d6d345f90cf75c9f29d87b35e817a07c52d
suyash-more <suyashmore789@gmail.com>
2023-05-28 18:44:02 +0530

    token ring code mod

--- Assignment5/tokenRing.java
+++ Assignment5/tokenRing.java
@@ -1,41 +1,36 @@
 import java.util.Scanner;
 
 class TokenRing {
-    public static void main(String[] args) throws Throwable{    
+    public static void main(String[] args) throws Throwable {    
         System.out.println("Enter the number of processes: ");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter n : ");
+        System.out.print("Enter n : ");
         int n = sc.nextInt();
         int token = 0;
-        int ch = 1;
-        int sender, reciever;
+        int sender, receiver;
         String message;
         boolean message_pass = false;
         System.out.println("Initializing ring");
         for(int i = 0; i < n; i++) {
-            System.out.println(" " + i);                        
+            System.out.print(" " + i);                        
         }
-        System.out.println("Enter sender : ");
+        System.out.print("\nEnter sender : ");
         sender = sc.nextInt();
-        System.out.println("Enter reciever : ");
-        reciever = sc.nextInt();
+        System.out.print("Enter receiver : ");
+        receiver = sc.nextInt();
         sc.nextLine();
-        System.out.println("Enter message : ");
+        System.out.print("Enter message : ");
         message = sc.nextLine();
-        for(token = 0; token < n; token++) {
-            System.out.println("Token at process " + token);
-            if(token == sender) {
-                System.out.println("Sender found");
-                System.out.println("Attaching message");  
-                message_pass = true;              
-            } else if (token == reciever) {
-                System.out.println("Token arrived at reciever");
-                System.out.println("Message " + message + " recieved");
-                message_pass = false;                
-            }
-            if(message_pass) {
-                System.out.println(token + " passing message " + message + " to " + (token + 1)%n);
-            }
+
+        System.out.println("Token Cicrulation Started");
+        for(int i = token; i != sender; i++) {
+            System.out.print(" " + i + "->");
         }
+        System.out.println(" " + sender);
+        System.out.println(sender + " Sending Message : " + message);
+        for(int i = sender + 1; i != receiver; i = (i+1) % n){
+            System.out.println("Message '" + message + "' forwarded by " + i);
+        }
+        System.out.println("Receiver " + receiver + " Received Message: " + message);
     }
 }
